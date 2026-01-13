3f997330e6f772310b3d1b8075e03393a3043606
Sudhanshu-06 <sudhanshu767778@gmail.com>
2026-01-13 22:45:15 +0530

    done task

---
+++ Chorot_1.0/JavaScript/basic/hello.js
@@ -0,0 +1,20 @@
+console.log("Hello from chai");
+
+function printChai(){
+    console.log("hello chai")
+}
+
+function bringBrush(noOfBrush){
+    console.log(`Hanji,le aaye ${noOfBrush} brush`)
+}
+
+function addTwo(num1,num2){
+    return num1+num2;
+}
+
+
+printChai();
+bringBrush(14);
+addTwo(1,1)
+
+console.log(addTwo(2,2))
